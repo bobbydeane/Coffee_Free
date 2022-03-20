82f03e3d5b102a187bcc19bfa7bf016786c6640b
bobbydeane <robertdeane1992@gmail.com>
2022-03-20 11:35:30 +0000

    Add Comment and Post admin, add PostList view

--- community/views.py
+++ community/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView): #will inherit generic list view
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on') # status =1 filters by published posts / ordered by oldest first
+    template_name = 'index.html' # view will render our Html file
+    paginate_by= 6 # introduce page navigation after 6 posts
