68d19c3d5a4815640dedc072cab488a20e23154a
bobbydeane <robertdeane1992@gmail.com>
2022-03-21 23:12:13 +0000

    Add base.html

--- community/views.py
+++ community/views.py
@@ -5,5 +5,5 @@ from .models import Post
 class PostList(generic.ListView): #will inherit generic list view
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on') # status =1 filters by published posts / ordered by oldest first
-    template_name = 'index.html' # view will render our Html file
-    paginate_by= 6 # introduce page navigation after 6 posts
+    template_name = "index.html" # view will render our Html file
+    paginate_by = 6 # introduce page navigation after 6 posts
