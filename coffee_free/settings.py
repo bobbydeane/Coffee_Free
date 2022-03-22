80ff63c08b9c21e0a8828c8107847875833fe5b9
bobbydeane <robertdeane1992@gmail.com>
2022-03-22 22:38:04 +0000

    Add basic sign up form with all auth, added sign up and log out url to base html

--- coffee_free/settings.py
+++ coffee_free/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     "bootstrap3",
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -49,6 +53,12 @@ INSTALLED_APPS = [
     'community',
     'coffee_free'
 ]
+
+# 1 site sing 1 db
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
