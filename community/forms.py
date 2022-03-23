1620c229d0ef0f541f1929cea15b682362d37a9b
bobbydeane <robertdeane1992@gmail.com>
2022-03-23 23:56:27 +0000

    Add crispy forms and comment functionality

---
+++ community/forms.py
@@ -0,0 +1,8 @@
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',) # tells what fields we want on our form
--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 class PostList(generic.ListView): #will inherit generic list view
     model = Post
@@ -24,6 +25,41 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST) # Gets data from our form 
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email #set username automatically from user
+            comment_form.instance.name = request.user.username #set email automatically from user
+            comment =comment_form.save(commit=False)
+            comment.post = post #assigns comment to post
+            comment.save() #saves comment
+
+        else:
+            comment_form = CommentForm() #if form not valid then leave an empty comment
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
