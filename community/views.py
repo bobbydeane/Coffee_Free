7c00ccaa5261b23d539089dbc2c9c2985272cf5b
bobbydeane <robertdeane1992@gmail.com>
2022-03-28 20:52:11 +0000

    fix the post detail page

--- community/views.py
+++ community/views.py
@@ -8,8 +8,7 @@ class PostList(generic.ListView): #will inherit generic list view
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on') # status =1 filters by published posts / ordered by oldest first
     template_name = "index.html" # view will render our Html file
-    paginate_by = 6 # introduce page navigation after 6 posts
-
+    paginate_by = 6 # introduce page navigation after 1 posts
 
     
 class PostDetail(View):
